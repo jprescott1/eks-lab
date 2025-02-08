858dbb704477bb8874c55713ef89e9ebc8e83247
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-08 12:21:45 -0600

    fix file name

--- iam-policy.tf
+++ iam-policy.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_policy" "eks_role_policy" {
   name   = "EKSClusterIAMAccess"
-  policy = templatefile("${path.module}/policies/iam-create-role-policy.json", { AWS_ACCOUNT_ID = var.aws_account_id })
+  policy = templatefile("${path.module}/policies/eks-cluster-inline-policy.json", { AWS_ACCOUNT_ID = var.aws_account_id })
 }
